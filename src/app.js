a84093e9828ba250c735cbf6e9ce0559cb30b29b
Dag4 <dul.dagmara@gmail.com>
2021-02-07 12:02:45 +0100

    remove unnec lines

--- src/app.js
+++ src/app.js
@@ -1,20 +1,7 @@
 import { route } from './router';
-import { testTemplate, addTemplate } from './test';
 import view1 from '../src/view1.html';
 
 $(document).ready(function () {
-
-// var link = document.createElement('link');
-// link.rel = 'import';
-// link.href = 'form.html';
-
-// link.onload = function(e){
-//   $('#app').append(link);
-// }
-
-// testTemplate();
-// $('#app').load('./template1.html')
-// addTemplate();
 
 // route('/', 'home', function() {
 //   this.where = 'here';
@@ -22,7 +9,7 @@ $(document).ready(function () {
 
 $('head').append(view1)
 
-console.log($('#app'));
+
 $('#app').css('width','50px');
 $('#app').css('height','50px');
 $('#app').css('background-color','red');
@@ -50,9 +37,7 @@ route('/ex2', 'example2', function() {
 route('/ex3', 'example3', function() {
   this.title1 = 'Formularz';
 });
-//  route('/form','form1',function(){
-//    this.title2="form";
-//  })
+
 
 route('*', '404', function () {});
 });
