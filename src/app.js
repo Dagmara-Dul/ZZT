44d9408d247f45a7c455db5bdfdb3c786fea7a4b
Dag4 <dul.dagmara@gmail.com>
2021-02-08 18:45:56 +0100

    login style

--- src/app.js
+++ src/app.js
@@ -4,16 +4,12 @@ require('../src/main.css');
 
 $(document).ready(function () {
 
-
-
 $('head').append(view1)
 
-let arrayEmail =[];
-
 route('/', 'view1', function() {
-  this.formTitle = 'Log in';
-  this.emailFormLabel = 'Your email address';
-  this.passwordFormLabel='Your password';
+  this.formTitle = 'Welcome';
+  this.emailFormLabel = 'Email';
+  this.passwordFormLabel='Password';
   this.submit='log in';
   
 
@@ -22,21 +18,15 @@ route('/', 'view1', function() {
     let emailValue = e.target.value.trim();
   });
 
- 
 
   var passwordObj = this.$on('#password','change', (e)=>{
     console.log(e.target.value)
-    let passwordValue = e.target.value.trim();return passwordValue
+    let passwordValue = e.target.value.trim();
   });
-
-
   
 
   this.$on('#login-form','submit', (event)=>{
     event.preventDefault();
-    console.log('submit!');
-    console.log(passwordObj)
-    console.log(this)
     this.$refresh();
   })
 });
@@ -61,55 +51,3 @@ route('/ex3', 'example3', function() {
 
 route('*', '404', function () {});
 });
-
-
-// import { route } from './router';
-// import {view1} from '../src/view1.html';
-
-// $(document).ready(function () {
-
-// // route('/', 'home', function() {
-// //   this.where = 'here';
-// // });
-
-// $('head').append(view1);
-
-
-// // $('#app').css('width','50px');
-// // $('#app').css('height','50px');
-// // $('#app').css('background-color','red');
-
-// // route('/', 'view1', function() {
-// //   this.formTitle = 'heredupa';
-// //   this.emailFormLabel = 'daga';
-// //   this.passwordFormLabel='daga1';
-// //   this.submit='submit-button';
-// // });
-
-// route('/', 'view1', function() {
-//   this.formTitle = 'here';
-// });
-
-// route('/ex1', 'example1', function() {
-//   this.title = 'Example 1';
-// });
-
-// route('/ex2', 'example2', function() {
-//   this.title = 'Example 2';
-//   this.counter = 0;
-//   this.$on('.my-button', 'click', () => {
-//     this.counter += 1;
-//     this.$refresh();
-//   });
-// });
-
-// route('/ex3', 'example3', function() {
-//   this.title1 = 'Formularz';
-// });
-
-
-// route('*', '404', function () {});
-
-
-
-// });
--- webpack.config.js
+++ webpack.config.js
@@ -27,14 +27,6 @@ module.exports = {
           }
         }
       },
-      // {
-      //   test:/\.s[ac]ss$/i,
-      //   use: [
-      //     "style-loader",
-      //     "css-loader",
-      //     "sass-loader"
-      //   ]
-      // },
       {
         test:/\.css/,
         use: [
